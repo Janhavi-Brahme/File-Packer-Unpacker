f8c009ccb456a4daf04e2c74e5503d18e6847eb6
Janhavi-Brahme <janhavi.a.brahme@gmail.com>
2024-07-27 16:11:17 +0530

    File Packer And Unpacker GUI

---
+++ Final_Packer_Unpacker_GUI/Demo/Program401.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+class Program401
+{
+    public static void main(String Ar[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter File Name");
+        String FileName = sobj.nextLine();
+
+        System.out.println("File name is : "+FileName);
+    }
+}
